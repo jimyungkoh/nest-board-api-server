fc6513687362b241fc222c1526a39d45e5e1043b
jimyungkoh <kojimyung1206@naver.com>
2024-06-13 20:05:02 +0900

    feat: 설정 및 TypeORM 모듈 추가
    - ConfigModule을 글로벌 모듈로 추가
    - TypeOrmConfigModule 추가
    - TypeOrmConfigService 생성 및 TypeORM 설정 로직 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { TypeOrmConfigModule } from './database/type-orm.config.module';
 import { AuthModule } from './domains/auth/auth.module';
 import { PostsModule } from './domains/posts/posts.module';
 import { UsersModule } from './domains/users/users.module';
 
 @Module({
-  imports: [AuthModule, PostsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmConfigModule,
+    AuthModule,
+    PostsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
---
+++ src/database/type-orm.config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: (this.configService.get<string>('DB_TYPE') as 'mysql') || 'mysql',
+      host: this.configService.get<string>('DB_HOST') || 'localhost',
+      port: this.configService.get<number>('DB_PORT') || 3306,
+      username: this.configService.get<string>('DB_USERNAME') || 'root',
+      password: this.configService.get<string>('DB_PASSWORD') || 'root',
+      database: this.configService.get<string>('DB_DATABASE') || 'test',
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: this.configService.get<boolean>('DB_SYNCHRONIZE') || false,
+    };
+  }
+}
