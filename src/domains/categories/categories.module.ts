3edd76029c2709db920289eb8e095cdf8f867e38
jimyungkoh <kojimyung1206@naver.com>
2024-06-14 17:07:00 +0900

    chore: 카테고리 모듈의 TypeOrmModule imports 수정
    - 카테고리 모듈 내부에서 Post 엔티티를 직접적으로 사용하지 않으므로 임포트 삭제

--- src/domains/categories/categories.module.ts
+++ src/domains/categories/categories.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from '../posts/entities';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { Category } from './entities';
@@ -8,7 +7,7 @@ import { Category } from './entities';
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService],
-  imports: [TypeOrmModule.forFeature([Category, Post])],
+  imports: [TypeOrmModule.forFeature([Category])],
   exports: [CategoriesService],
 })
 export class CategoriesModule {}
