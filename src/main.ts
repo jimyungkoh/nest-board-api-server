9930ffc44cc0b0e53be31b775c19e81d5c7a9f26
jimyungkoh <kojimyung1206@naver.com>
2024-06-14 21:58:43 +0900

    feat: 성공 응답 인터셉터 및 전역 설정 추가
    - SuccessResponseInterceptor 추가: error가 발생하지 않은 모든 응답을 성공 형식으로 포매팅 후 반환
    - main.ts 수정: 전역 ValidationPipe 및 SuccessResponseInterceptor 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SuccessResponseInterceptor } from './common/interceptors/success-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
