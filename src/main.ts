fcfa6e5c8a8c9f03e8c9d6141b81a3a400b3361e
jimyungkoh <kojimyung1206@naver.com>
2024-06-17 05:02:42 +0900

    feat: Swagger 설정 추가
    - `SwaggerModule` 및 `DocumentBuilder`를 사용하여 Swagger 설정 추가
    - API 문서화 제목, 설명, 버전 설정
    - 쿠키 인증 추가
    - `/api` 경로에 Swagger UI 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/grobal-exception.filter';
@@ -15,6 +16,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('게시판 API 서버')
+    .setDescription('게시판 API 서버')
+    .setVersion('1.0')
+    .addCookieAuth('Authorization')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get('PORT') || 3000, '0.0.0.0');
 }
